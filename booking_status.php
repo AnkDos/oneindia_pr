182255d2e01fa6d024c8011b4e6bd0ca029ee106
Ankur <ankurpan96@gmail.com>
2018-08-23 00:30:14 +0000

    finalized

--- booking_status.php
+++ booking_status.php
@@ -1,8 +1,8 @@
 <?php
 require 'con.php';
-$b_id=$_GET['book_id'];
-$query = mysqli_query($con,"select flight_id,booking_id,status from bookings where booking_id ='$b_id'");
+$b_id=$_GET['book_id'];    //grabbing booking id from get method
+$query = mysqli_query($con,"select flight_id,booking_id,status from bookings where booking_id ='$b_id'"); 
 $fetch = mysqli_fetch_assoc($query);
-$json = json_encode($fetch);
+$json = json_encode($fetch);    //converting the details in to json
 print_r($json);
 ?>
