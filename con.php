e158ff66b0652bce7485ed3f4dc6da2e7c4dd043
Ankur <ankurpan96@gmail.com>
2018-08-23 00:28:44 +0000

    added comments

--- con.php
+++ con.php
@@ -3,12 +3,13 @@
 //Created By Ankur(AnkDos)
 //dummy username and password given for security reasons 
 
-define('DBHOST', 'localhost');
+//defining all the creds
+ define('DBHOST', 'localhost');
  define('DBUSER', 'USERNAME HERE');
  define('DBPASS', 'KEY HERE');
  define('DBNAME', 'DATABASE NAME');
- $con = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
- if ( !$con ) {
-  echo ("Connection Failed");
+ $con = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME); //establishing connection
+ if ( !$con ) {                  
+  echo ("Connection Failed");     // if error occurs   
  }
 ?>
