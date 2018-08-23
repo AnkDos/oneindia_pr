177382b0ac0e530ea37cf53ebbd4324e55f34590
AnkDos <ankurpan96@gmail.com>
2018-08-23 02:49:49 +0000

    Added Sections

--- index.php
+++ index.php
@@ -1,3 +1,27 @@
+<?php
+//php here
+?>
 <html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+        crossorigin="anonymous">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+        #adr {
+            height: 200vh;
+        }
 
+        #dr {
+            height: 200vh;
+        }
+</style>
+        </head>
+
+        <section id="adr">
+
+        </section>
+
+        <section id="dr">
+
+        </section>
 </html>
